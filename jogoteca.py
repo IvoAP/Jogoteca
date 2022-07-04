9c4839fc74151774c8b965a788b0edee80b66332
Ivo Pimenta <ivoaguiarpimenta@gmail.com>
2022-07-04 02:03:53 -0300

    Inserção dinamica de jogos

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,11 @@ class Jogo:
         self.nome = nome
         self.categoria = categoria
         self.console = console
+
+jogo1 = Jogo('tetrix' , 'Puzzle', 'Atari')
+jogo2 = Jogo ('God of War', 'Hackingslash', 'Play Station 2')
+jogo3 = Jogo('Dragon Ball Fighter Z', 'Luta', 'Play Station 3, Xbox, Computador')
+lista = [jogo1, jogo2, jogo3]
 
 @app.route('/inicio')
 def ola():
@@ -19,6 +24,16 @@ def ola():
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo ='Novo Jogo')
+
+@app.route('/criar', methods = ['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome,categoria,console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista)
+
 
 app.run(host = '0.0.0.0', port = 8080)
 
