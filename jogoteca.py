ea2672810da756f61623b5e7501b3a3d12e88ee9
Ivo Pimenta <ivoaguiarpimenta@gmail.com>
2022-07-04 01:32:36 -0300

    Mostrando view da Lista de jogos de forma dinamica

--- jogoteca.py
+++ jogoteca.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    jogo1 = Jogo('tetrix' , 'Puzzle', 'Atari')
+    jogo2 = Jogo ('God of War', 'Hackingslash', 'Play Station 2')
+    jogo3 = Jogo('Dragon Ball Fighter Z', 'Luta', 'Play Station 3, Xbox, Computador')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html',titulo = 'Jogos', jogos = lista)
 
 app.run(host = '0.0.0.0', port = 8080)
 
